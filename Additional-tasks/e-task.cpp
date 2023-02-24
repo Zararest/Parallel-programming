18bc466f2846571b63a8ba174a98d8293b18da81
UwU <iishumakov@gmail.com>
2023-02-25 01:34:37 +0300

    Working version

--- Additional-tasks/e-task.cpp
+++ Additional-tasks/e-task.cpp
@@ -1,10 +1,17 @@
+//mpirun -np 4 ./e-calc > ../result
+
 #include <boost/multiprecision/gmp.hpp> 
 #include <vector>
+#include <iostream>
 
 #include "Manager.h"
 #include "gmpxx.h"
 
-constexpr unsigned getPrecision(){ return 20; }
+#define SERIES_SIZE 1000000
+#define ITER_SIZE 10
+
+
+constexpr unsigned getPrecision(){ return 1000; }
 constexpr unsigned FloatSize = getPrecision();
 constexpr unsigned IntSize = getPrecision();
 
@@ -22,34 +29,39 @@ class Worker {
     return (MPI.getPID() + 1) % MPI.getMPIGroupSize();
   }
 
-  BigInt getAdditionalWork() {
+  BigFloat getAdditionalWork() {
     assert(MPI.getPID() == 0);
-    auto AdditionalSeqBeg = SizeOfSeq * NumOfIter + 1;
-    BigInt AdditionalRes = 1;
-    for (unsigned SeqPos = 0; AdditionalSeqBeg + SeqPos <= N; SeqPos++)
-      AdditionalRes = AdditionalRes * (AdditionalSeqBeg + SeqPos);
+    auto AdditionalSeqBeg = (SizeOfSeq * NumOfWorkers) * NumOfIter + 1;
+    BigFloat AdditionalRes = 1, CurFactorial = 1;
+    #ifdef DEBUG_GROUP
+    std::cout << "Additional: {" << AdditionalSeqBeg << ", " << N << "}\n";
+    #endif
+    for (unsigned SeqPos = 0; AdditionalSeqBeg + SeqPos <= N; SeqPos++) {
+      CurFactorial = CurFactorial / (AdditionalSeqBeg + SeqPos);
+      AdditionalRes += CurFactorial;
+    }
     return AdditionalRes * recvRes(getPrevWorkerID());
   }
 
-  BigInt recvRes(unsigned SrcID) {
+  BigFloat recvRes(unsigned SrcID) {
     auto PrevFactorial =  MPI.recv<std::string>(SrcID, DEFAULT_TAG, getGroup(MPIGroup::World));
-    return BigInt{PrevFactorial};
+    return BigFloat{PrevFactorial};
   }
 
-  void sendCurRes(BigInt Res, 
+  void sendCurRes(BigFloat Res, 
                   unsigned DestID) {
     MPI.send(Res.str(), DestID, DEFAULT_TAG, getGroup(MPIGroup::World));
   }
 
-  BigInt calcCachedWork() {
-    BigInt Res = 0;
+  BigFloat calcCachedWork() {
+    BigFloat Res = 0;
     for (auto It : CachedWork)
       Res = Res + It;
     return Res; 
   }
 
-  BigInt getAllMainWork() {
-    BigInt Res = 0;
+  BigFloat getAllMainWork() {
+    BigFloat Res = 0;
     for (auto WorkerId = 1; WorkerId < MPI.getMPIGroupSize(); WorkerId++)
       Res = Res + recvRes(WorkerId);
     return Res + calcCachedWork();
@@ -65,10 +77,17 @@ class Worker {
     auto WorkerID = MPI.getPID();
     auto AllWorkPerIter = NumOfWorkers * SizeOfSeq;
     auto CurSeqBegin = CurIt * AllWorkPerIter + WorkerID * SizeOfSeq + 1;
-    BigInt SeqRes = 1;
-    for (unsigned SeqPos = 0; SeqPos < SizeOfSeq; SeqPos++) 
-      SeqRes = SeqRes * (CurSeqBegin + SeqPos);
-    sendCurRes(recvRes(getPrevWorkerID()) * SeqRes, getNextWorkerID()); 
+    BigFloat SeqRes = 0, CurFactorial = 1;
+    #ifdef DEBUG_GROUP
+    std::cout << "Group: {" << CurSeqBegin << ", " << CurSeqBegin + SizeOfSeq - 1 << "}\n"; 
+    #endif
+    for (unsigned SeqPos = 0; SeqPos < SizeOfSeq; SeqPos++) {
+      CurFactorial = CurFactorial / (CurSeqBegin + SeqPos);
+      SeqRes += CurFactorial;
+    }  
+    auto PrevFactorial = recvRes(getPrevWorkerID());
+    CachedWork.push_back(PrevFactorial * SeqRes);
+    sendCurRes(PrevFactorial * CurFactorial, getNextWorkerID()); 
   }
 
 public:
@@ -77,8 +96,13 @@ public:
          unsigned SizeOfIt) : MPI{MPIMan},
                               N{SeriesSize},
                               SizeOfSeq{SizeOfIt} {
-    NumOfIter = SeriesSize / SizeOfIt;
     NumOfWorkers = MPIMan.getMPIGroupSize();
+    NumOfIter = SeriesSize / (SizeOfIt * NumOfWorkers);
+  }
+
+  void start() {
+    if (MPI.getPID() == MPI.getMPIGroupSize() - 1) 
+      sendCurRes(BigFloat{"1"}, 0);
   }
 
   void calc() {
@@ -89,11 +113,14 @@ public:
 
   BigFloat getReult() {
     assert(MPI.getPID() == 0);
-    return getAllMainWork() + getAdditionalWork();
+    auto Main = getAllMainWork();
+    auto Additional = getAdditionalWork();
+    std::cout << "Main:" << Main << " Aditional: " << Additional << std::endl;
+    return Main + Additional;
   }
 
 private:
-  std::vector<BigInt> CachedWork;
+  std::vector<BigFloat> CachedWork;
   unsigned SizeOfSeq = 0;     //work per iteration per worker
   unsigned NumOfIter = 0;     //number of sequences
   unsigned NumOfWorkers = 1;    
@@ -101,11 +128,14 @@ private:
   unsigned N;                 //number of elements in e series
 };
 
-
 int main() {
-  BigFloat f{"1.1232342342342342423423423442"};
-  BigInt i{"123123131312313123123123123123131231312313123131231332312"};
-
-  std::cout << "Float:" << f.str(10000) << std::endl;
-  std::cout << "Size: " << i.str(1000) << std::endl;
+  MPIManager MPI;
+  Worker Worker{MPI, SERIES_SIZE, ITER_SIZE};
+  Worker.start();
+  Worker.calc();
+  if (MPI.getPID() == 0) {
+    BigFloat Res{"1"};
+    Res += Worker.getReult();
+    std::cout << Res.str() << std::endl;
+  }
 }
